28e366d3b30874f5d4389203c78689b90f2e302b
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-03-23 17:05:50 +0500

    added post method

--- src/server.js
+++ src/server.js
@@ -5,4 +5,37 @@ const mongoose = require("mongoose");
 app.use(express.json());
 mongoose.connect("mongodb://localhost:27017/mongodb");
 
+const bookSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  price: Number,
+});
+
+const bookModel = mongoose.model("Books", bookSchema);
+
+app.get("/", async (_, res) => {
+  const foundBooks = await bookModel.find();
+  console.log(foundBooks);
+  res.send("Books");
+});
+
+app.post("/post", async (req, res) => {
+  try {
+    const { name, author, price } = req.body;
+    const newBook = await bookModel.create({ name, author, price });
+    console.log(newBook);
+
+    res.send("added new Book");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.listen(9000, console.log(9000));
